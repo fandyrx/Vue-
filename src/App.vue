3c44a95dd43eacfb30f551c552fab5261612c47a
Fandy <290104314@qq.com>
2022-07-27 17:36:42 +0800

    第二次上传，详情页基本完成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <keep-alive>
+    <keep-alive exclude="Detail">
       <router-view></router-view>
    </keep-alive>
       <main-tab-bar></main-tab-bar>
@@ -29,8 +29,4 @@ export default {
 <style>
    @import 'assets/css/base';
  
-
-
-
-  
 </style>
